0b992616288c37233ca314803d52c44bbf1a0439
Jillo Wario <willywario0@gmail.com>
2024-01-05 20:50:57 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const main = document.getElementById("main");
+
+document.body.removeChild(main);
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "WARIO is the champion";
